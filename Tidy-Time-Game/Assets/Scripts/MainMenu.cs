96a18c0f794cf5ff9e0dabe167660f55cf6f6ff3
Darren <darrenawf@gmail.com>
2025-02-24 16:36:00 -0500

    button for homework

--- Tidy-Time-Game/Assets/Scripts/MainMenu.cs
+++ Tidy-Time-Game/Assets/Scripts/MainMenu.cs
@@ -12,6 +12,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadSceneAsync(1);
     }
 
+    public void PlayHomework()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+
     public void QuitGame()
 {
     Application.Quit();
