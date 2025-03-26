3cba8d90d8064b23ae425233a4fe8bdefc43a5af
Darren <darrenawf@gmail.com>
2025-03-25 21:39:56 -0400

    math game complete

--- Tidy-Time-Game/Assets/Scripts/MathChore.cs
+++ Tidy-Time-Game/Assets/Scripts/MathChore.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class MathChore : MonoBehaviour
 {
     // Question and Answer objects for 5 questions
     public TMP_Text question1Text, question2Text, question3Text, question4Text, question5Text;
     public TMP_InputField answer1Input, answer2Input, answer3Input, answer4Input, answer5Input;
+    
+    // UI elements for individual checks
+    public Button check1Button, check2Button, check3Button, check4Button, check5Button;
+    
+    // Color settings
+    public Color correctColor = Color.green;
+    public Color incorrectColor = Color.red;
+    public Color defaultColor = Color.white;
+    public Color disabledColor = new Color(0.8f, 0.8f, 0.8f); // Light grey for disabled state
+    [Range(0, 1)] public float highlightIntensity = 0.7f;
+
+    // Track which questions are completed
+    private bool[] completedQuestions = new bool[5];
 
     private List<string> questions = new List<string>();
     private List<int> correctAnswers = new List<int>();
 
-    // Start is called before the first frame update
     void Start()
     {
         GenerateQuestions();
@@ -24,21 +37,25 @@ public class MathChore : MonoBehaviour
         AddInputValidation(answer3Input);
         AddInputValidation(answer4Input);
         AddInputValidation(answer5Input);
+
+        // Add listeners for individual check buttons
+        check1Button.onClick.AddListener(() => CheckAnswer1());
+        check2Button.onClick.AddListener(() => CheckAnswer2());
+        check3Button.onClick.AddListener(() => CheckAnswer3());
+        check4Button.onClick.AddListener(() => CheckAnswer4());
+        check5Button.onClick.AddListener(() => CheckAnswer5());
     }
 
     void AddInputValidation(TMP_InputField inputField)
     {
-        // Add a listener to validate input as the user types
         inputField.onValueChanged.AddListener((value) =>
         {
-            if (value.Length > 1)
+            if (value.Length > 0)
             {
-                // If more than one character is entered, trim it to the first character
                 inputField.text = value.Substring(0, 1);
             }
 
-            // Ensure the input is a valid digit between 1 and 9
-            if (!int.TryParse(inputField.text, out int parsedValue) || parsedValue < 1 || parsedValue > 9)
+            if (!int.TryParse(inputField.text, out int parsedValue) || parsedValue < 0 || parsedValue > 9)
             {
                 inputField.text = "";
             }
@@ -49,22 +66,22 @@ public class MathChore : MonoBehaviour
     {
         questions.Clear();
         correctAnswers.Clear();
+        completedQuestions = new bool[5];
         string[] operators = { "+", "-" };
 
-        for (int i = 0; i < 5; i++)  // Generates 5 questions
+        for (int i = 0; i < 5; i++)
         {
             string op = operators[Random.Range(0, operators.Length)];
-            int num1 = Random.Range(1, 10);
+            int num1 = Random.Range(1, 9);
             int num2 = 0;
 
-            // Ensure that num2 and num1 result in an answer between 1 and 9
             switch (op)
             {
                 case "+": 
-                    num2 = Random.Range(1, 10 - num1);  // Ensure the sum does not exceed 9
+                    num2 = Random.Range(1, 9 - num1);
                     break;
                 case "-": 
-                    num2 = Random.Range(1, num1);  // Ensure the result is greater than or equal to 1
+                    num2 = Random.Range(1, num1);
                     break;
             }
 
@@ -88,123 +105,152 @@ public class MathChore : MonoBehaviour
 
     void ShowAllQuestions()
     {
-        // Display all questions at once
         question1Text.text = questions[0];
         question2Text.text = questions[1];
         question3Text.text = questions[2];
         question4Text.text = questions[3];
         question5Text.text = questions[4];
 
-        // Clear all input fields
         answer1Input.text = "";
         answer2Input.text = "";
         answer3Input.text = "";
         answer4Input.text = "";
         answer5Input.text = "";
+
+        // Reset UI elements
+        ResetQuestionUI(0, answer1Input, check1Button);
+        ResetQuestionUI(1, answer2Input, check2Button);
+        ResetQuestionUI(2, answer3Input, check3Button);
+        ResetQuestionUI(3, answer4Input, check4Button);
+        ResetQuestionUI(4, answer5Input, check5Button);
+    }
+
+    void ResetQuestionUI(int index, TMP_InputField inputField, Button button)
+    {
+        completedQuestions[index] = false;
+        inputField.interactable = true;
+        button.interactable = true;
+        
+        UpdateButtonColors(button, defaultColor);
+    }
+
+    void LockCorrectAnswer(TMP_InputField inputField, Button button)
+    {
+        inputField.interactable = false;
+        button.interactable = false;
+        
+        // Set the disabled color to our correct color
+        ColorBlock colors = button.colors;
+        colors.disabledColor = correctColor;
+        button.colors = colors;
+    }
+
+    void UpdateButtonColors(Button button, Color color)
+    {
+        ColorBlock colors = button.colors;
+        colors.normalColor = color;
+        colors.highlightedColor = Color.Lerp(color, Color.white, highlightIntensity);
+        colors.pressedColor = Color.Lerp(color, Color.black, highlightIntensity);
+        colors.selectedColor = color;
+        button.colors = colors;
+    }
+
+    // Individual check functions
+    public void CheckAnswer1()
+    {
+        if (completedQuestions[0]) return;
+        bool isCorrect = CheckSingleAnswer(0, answer1Input);
+        if (isCorrect)
+        {
+            LockCorrectAnswer(answer1Input, check1Button);
+            completedQuestions[0] = true;
+        }
+        else
+        {
+            UpdateButtonColors(check1Button, incorrectColor);
+        }
+    }
+
+    public void CheckAnswer2()
+    {
+        if (completedQuestions[1]) return;
+        bool isCorrect = CheckSingleAnswer(1, answer2Input);
+        if (isCorrect)
+        {
+            LockCorrectAnswer(answer2Input, check2Button);
+            completedQuestions[1] = true;
+        }
+        else
+        {
+            UpdateButtonColors(check2Button, incorrectColor);
+        }
+    }
+
+    public void CheckAnswer3()
+    {
+        if (completedQuestions[2]) return;
+        bool isCorrect = CheckSingleAnswer(2, answer3Input);
+        if (isCorrect)
+        {
+            LockCorrectAnswer(answer3Input, check3Button);
+            completedQuestions[2] = true;
+        }
+        else
+        {
+            UpdateButtonColors(check3Button, incorrectColor);
+        }
+    }
+
+    public void CheckAnswer4()
+    {
+        if (completedQuestions[3]) return;
+        bool isCorrect = CheckSingleAnswer(3, answer4Input);
+        if (isCorrect)
+        {
+            LockCorrectAnswer(answer4Input, check4Button);
+            completedQuestions[3] = true;
+        }
+        else
+        {
+            UpdateButtonColors(check4Button, incorrectColor);
+        }
+    }
+
+    public void CheckAnswer5()
+    {
+        if (completedQuestions[4]) return;
+        bool isCorrect = CheckSingleAnswer(4, answer5Input);
+        if (isCorrect)
+        {
+            LockCorrectAnswer(answer5Input, check5Button);
+            completedQuestions[4] = true;
+        }
+        else
+        {
+            UpdateButtonColors(check5Button, incorrectColor);
+        }
+    }
+
+    private bool CheckSingleAnswer(int questionIndex, TMP_InputField inputField)
+    {
+        if (int.TryParse(inputField.text, out int userAnswer))
+        {
+            if (userAnswer >= 0 && userAnswer <= 9 && userAnswer == correctAnswers[questionIndex])
+            {
+                Debug.Log($"Question {questionIndex + 1}: Correct!");
+                return true;
+            }
+        }
+        Debug.Log($"Question {questionIndex + 1}: Incorrect!");
+        return false;
     }
 
     public void CheckAnswers()
     {
-        bool allCorrect = true;
-
-        // Check answers for each question
-        if (int.TryParse(answer1Input.text, out int userAnswer1))
-        {
-            if (userAnswer1 < 1 || userAnswer1 > 9 || userAnswer1 != correctAnswers[0])
-            {
-                allCorrect = false;
-                Debug.Log("Question 1: Incorrect or out of range!");
-            }
-            else
-            {
-                Debug.Log("Question 1: Correct!");
-            }
-        }
-        else
-        {
-            allCorrect = false;
-            Debug.Log("Question 1: Invalid input!");
-        }
-
-        if (int.TryParse(answer2Input.text, out int userAnswer2))
-        {
-            if (userAnswer2 < 1 || userAnswer2 > 9 || userAnswer2 != correctAnswers[1])
-            {
-                allCorrect = false;
-                Debug.Log("Question 2: Incorrect or out of range!");
-            }
-            else
-            {
-                Debug.Log("Question 2: Correct!");
-            }
-        }
-        else
-        {
-            allCorrect = false;
-            Debug.Log("Question 2: Invalid input!");
-        }
-
-        if (int.TryParse(answer3Input.text, out int userAnswer3))
-        {
-            if (userAnswer3 < 1 || userAnswer3 > 9 || userAnswer3 != correctAnswers[2])
-            {
-                allCorrect = false;
-                Debug.Log("Question 3: Incorrect or out of range!");
-            }
-            else
-            {
-                Debug.Log("Question 3: Correct!");
-            }
-        }
-        else
-        {
-            allCorrect = false;
-            Debug.Log("Question 3: Invalid input!");
-        }
-
-        if (int.TryParse(answer4Input.text, out int userAnswer4))
-        {
-            if (userAnswer4 < 1 || userAnswer4 > 9 || userAnswer4 != correctAnswers[3])
-            {
-                allCorrect = false;
-                Debug.Log("Question 4: Incorrect or out of range!");
-            }
-            else
-            {
-                Debug.Log("Question 4: Correct!");
-            }
-        }
-        else
-        {
-            allCorrect = false;
-            Debug.Log("Question 4: Invalid input!");
-        }
-
-        if (int.TryParse(answer5Input.text, out int userAnswer5))
-        {
-            if (userAnswer5 < 1 || userAnswer5 > 9 || userAnswer5 != correctAnswers[4])
-            {
-                allCorrect = false;
-                Debug.Log("Question 5: Incorrect or out of range!");
-            }
-            else
-            {
-                Debug.Log("Question 5: Correct!");
-            }
-        }
-        else
-        {
-            allCorrect = false;
-            Debug.Log("Question 5: Invalid input!");
-        }
-
-        if (allCorrect)
-        {
-            Debug.Log("All questions are correct!");
-        }
-        else
-        {
-            Debug.Log("Some questions were incorrect.");
-        }
+        if (!completedQuestions[0]) CheckAnswer1();
+        if (!completedQuestions[1]) CheckAnswer2();
+        if (!completedQuestions[2]) CheckAnswer3();
+        if (!completedQuestions[3]) CheckAnswer4();
+        if (!completedQuestions[4]) CheckAnswer5();
     }
 }
